04e816e190500f43a14328fcca2604d3e5492085
bernazga-netizen <bernazga@gmail.com>
2026-05-18 13:09:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const pool = new Pool({
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Crear tabla de auditoría si no existe
 pool.query(`
